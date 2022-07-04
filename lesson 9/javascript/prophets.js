068e9950315e2b4ceb42557c75669a9be5f752ed
byui-programmer-96 <bam21002@byui.edu>
2022-07-04 10:52:52 -0500

    chnageas

--- lesson 9/javascript/prophets.js
+++ lesson 9/javascript/prophets.js
@@ -5,4 +5,12 @@ fetch(requestURL)
       })
       .then(function (jsonObject) {
         console.table(jsonObject);  // temporary checking for valid response and data parsing
+        const prophets = jsonObject['prophets'];
       });
+    for (let i = 0; i < prophets.length; i++ ) {(how would you do this with a foreach method?)
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+        card.appendChild(h2);
+        document.querySelector('div.cards').appendChild(card);}
+        image.setAttribute('src', prophets[i].imageurl);
