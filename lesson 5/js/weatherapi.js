942a3283f4d92326a672444f88503eb4e6dbab8f
byui-programmer-96 <bam21002@byui.edu>
2022-07-06 10:37:46 -0500

    tests

--- lesson 5/js/weatherapi.js
+++ lesson 5/js/weatherapi.js
@@ -1,6 +1,11 @@
-const apiURL = "https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={fc604e0bab14243d24f022dc6e463c28}";
+const apiURL = "http://api.openweathermap.org/data/3.0/onecall/timemachine?lat=39.099724&lon=-94.578331&dt=1643803200&appid={fc604e0bab14243d24f022dc6e463c28}";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
   });
+  const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+  const desc = jsObject.weather[0].description;  // note how we reference the weather array
+  document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+  document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+  document.getElementById('icon').setAttribute('alt', desc);
