7188e3c789fa3a917ae29c174a6ea58882d3225b
byui-programmer-96 <bam21002@byui.edu>
2022-07-06 10:09:33 -0500

    js

--- lesson 5/js/weatherapi.js
+++ lesson 5/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={fc604e0bab14243d24f022dc6e463c28}";
+const apiURL = "https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={fc604e0bab14243d24f022dc6e463c28}";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
