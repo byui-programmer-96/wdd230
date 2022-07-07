6c68d6d21f993872c823a2d01d407c17d42308fc
byui-programmer-96 <bam21002@byui.edu>
2022-07-07 16:50:31 -0500

    .....

--- lesson 5/js/weatherapi.js
+++ lesson 5/js/weatherapi.js
@@ -1,8 +1,9 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?lat=42.096291&lon=-111.876640&appid=6289b54b514cc72eac43e624d0af5cc";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=6289b54b514cc72eac43e624d0af5cc4&units=Fahrenheit(imperial)";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
   });
   const imagesrc = 'http://openweathermap.org/img/wn/10d@2x.png' + jsObject.weather[0].icon + '.png';  // note the concatenation
   const desc = jsObject.weather[0].description;  // note how we reference the weather array
