59bb335870db7ef7202abbcbfbf0da1756f8cdd7
byui-programmer-96 <bam21002@byui.edu>
2022-07-09 08:55:38 -0500

    js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const d = new Date();
 document.getElementById("currentDate").innerHTML = d;
+
+let Day = new Date().getDay();
+
+function weekDay (Day) {
+    if (Day == 6){
+        document.querySelector('#banner').style.display = "block";
+    }
+    
+}
+
+weekDay(Day);
