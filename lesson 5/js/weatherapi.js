5976be5dea206bb83e3c8850685b08a783bfeb8b
byui-programmer-96 <bam21002@byui.edu>
2022-07-13 12:08:25 -0500

    //

--- lesson 5/js/weatherapi.js
+++ lesson 5/js/weatherapi.js
@@ -1,4 +1,4 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=6289b54b514cc72eac43e624d0af5cc4&units=Fahrenheit&main.temp=Fahrenheit')
+fetch('https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=6289b54b514cc72eac43e624d0af5cc4&units=Imperial: Fahrenheit&main.temp=Imperial: Fahrenheit&main.humidity=Humidity, %&wind.speed=Imperial: miles/hour')
   .then((response) => {
     return response.json();
   })
