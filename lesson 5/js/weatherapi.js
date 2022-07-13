e0bc90af48012db1ef133fd0e87428d95501868c
byui-programmer-96 <bam21002@byui.edu>
2022-07-13 12:09:12 -0500

    js

--- lesson 5/js/weatherapi.js
+++ lesson 5/js/weatherapi.js
@@ -1,7 +1,13 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=6289b54b514cc72eac43e624d0af5cc4&units=Imperial: Fahrenheit&main.temp=Imperial: Fahrenheit&main.humidity=Humidity, %&wind.speed=Imperial: miles/hour')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  });
+let headersList = {
+  "Accept": "*/*",
+  "User-Agent": "Thunder Client (https://www.thunderclient.com)"
+ }
+ 
+ fetch("https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=6289b54b514cc72eac43e624d0af5cc4&units=Imperial: Fahrenheit&main.temp=Imperial: Fahrenheit&main.humidity=Humidity, %&wind.speed=Imperial: miles/hour", { 
+   method: "GET",
+   headers: headersList
+ }).then(function(response) {
+   return response.text();
+ }).then(function(data) {
+   console.log(data);
+ })
