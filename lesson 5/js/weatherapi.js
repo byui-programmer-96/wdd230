4b66470b78a76361f707974ed5d8ea5c9c54574b
byui-programmer-96 <bam21002@byui.edu>
2022-07-14 13:29:26 -0500

    js

--- lesson 5/js/weatherapi.js
+++ lesson 5/js/weatherapi.js
@@ -1,13 +1,19 @@
 let headersList = {
-  "Accept": "*/*",
+  Accept: "*/*",
   "User-Agent": "Thunder Client (https://www.thunderclient.com)"
- }
- 
- fetch("https://api.openweathermap.org/data/2.5/weather?q=Preston, USA&callback=test&appid=6289b54b514cc72eac43e624d0af5cc4", { 
-   method: "GET",
-   headers: headersList
- }).then(function(response) {
-   return response.text();
- }).then(function(data) {
-   console.log(data);
- })
+};
+
+fetch(
+  "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=6289b54b514cc72eac43e624d0af5cc4&units=Imperial",
+  {
+    method: "GET",
+    headers: headersList
+  }
+)
+  .then(function (response) {
+    return response.text();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+  
