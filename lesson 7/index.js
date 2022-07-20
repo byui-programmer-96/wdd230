edf0160869e23bee177574a211c71cde688b14ba
byui-programmer-96 <bam21002@byui.edu>
2022-07-20 15:18:23 -0500

    ..

--- lesson 7/index.js
+++ lesson 7/index.js
@@ -11,16 +11,3 @@ function weekDay (Day) {
 }
 
 weekDay(Day);
-let headersList = {
-    "Accept": "*/*",
-    "User-Agent": "Thunder Client (https://www.thunderclient.com)"
-   }
-   
-   fetch("https://api.openweathermap.org/data/2.5/weather?lat=20.510600&lon=-86.947350&appid=6289b54b514cc72eac43e624d0af5cc4&units=imperial", { 
-     method: "GET",
-     headers: headersList
-   }).then(function(response) {
-     return response.text();
-   }).then(function(data) {
-     console.log(data);
-   })
