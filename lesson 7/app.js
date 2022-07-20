0d5f4eaedef8fbcd3b27576df6e8a4fc624765a0
byui-programmer-96 <bam21002@byui.edu>
2022-07-20 15:00:50 -0500

    js fixes

--- lesson 7/app.js
+++ lesson 7/app.js
@@ -7,7 +7,7 @@ var button= document.querySelector('.submit');
 
 
 button.addEventListener('click', function(name){
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=6289b54b514cc72eac43e624d0af5cc4')
+fetch('https://api.openweathermap.org/data/2.5/weather?lat=20.510600&lon=-86.947350&appid=6289b54b514cc72eac43e624d0af5cc4&units=imperial'+input.value+'&appid=6289b54b514cc72eac43e624d0af5cc4')
 .then(response => response.json())
 .then(data => {
   var tempValue = data['main']['temp'];
