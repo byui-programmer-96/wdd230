da5609fe1b596c67c74943f13b21e582b18ac0c2
byui-programmer-96 <bam21002@byui.edu>
2022-07-20 15:59:13 -0500

    ...

--- lesson 7/app.js
+++ lesson 7/app.js
@@ -6,21 +6,22 @@ var clouds = document.querySelector('.clouds');
 var button= document.querySelector('.submit');
 
 
-button.addEventListener('click', function(name){
+button.addEventListener('click', function(){
 fetch('https://api.openweathermap.org/data/2.5/weather?lat=20.510600&lon=-86.947350&appid=6289b54b514cc72eac43e624d0af5cc4&units=imperial'+input.value+'&appid=6289b54b514cc72eac43e624d0af5cc4')
 .then(response => response.json())
 .then(data => {
   var tempValue = data['main']['temp'];
   var nameValue = data['name'];
   var descValue = data['weather'][0]['description'];
-  var humdValue = data['weather']['hum']
+  var humidityValue = data['humidity'];
 
   main.innerHTML = nameValue;
   desc.innerHTML = "Desc - "+descValue;
   temp.innerHTML = "Temp - "+tempValue;
-  input.value ="";
+  humidity.innerHTML = "humidity - "+humidityValue;
+  
 
 })
 
-.catch(err => alert("Wrong city name!"));
+.catch(() => alert("Wrong city name!"));
 })
